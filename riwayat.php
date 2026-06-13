7276cdfd041e28226518fe185c5677f6f481b1cd
dicky <dickyferdy5@gmail.com>
2026-06-13 07:49:24 +0700

    feat: Refactor sidebar implementation for consistent navigation across pages

--- riwayat.php
+++ riwayat.php
@@ -16,23 +16,10 @@ require_once __DIR__ . '/proses/proses_riwayat.php';
 
     <div class="dashboard-layout">
         
-        <aside class="sidebar">
-            <div class="sidebar-brand">
-                <div class="brand-icon"><iconify-icon icon="lucide:graduation-cap" width="24"></iconify-icon></div>
-                <div class="brand-text"><h3>CampusReport</h3><p>Akun Mahasiswa</p></div>
-            </div>
-            <ul class="nav-menu">
-                <li class="nav-item"><a href="dashboard.php"><iconify-icon icon="lucide:layout-dashboard" width="20"></iconify-icon> Dashboard</a></li>
-                <li class="nav-item"><a href="buat_laporan.php"><iconify-icon icon="lucide:file-edit" width="20"></iconify-icon> Buat Laporan</a></li>
-                <li class="nav-item"><a href="status_laporan.php"><iconify-icon icon="lucide:activity" width="20"></iconify-icon> Status Laporan</a></li>
-                <li class="nav-item active"><a href="riwayat.php"><iconify-icon icon="lucide:history" width="20"></iconify-icon> Riwayat</a></li>
-            </ul>
-            <div class="sidebar-bottom">
-                <ul class="nav-menu">
-                    <li class="nav-item"><a href="logout.php" class="logout-btn"><iconify-icon icon="lucide:log-out" width="20"></iconify-icon> Keluar</a></li>
-                </ul>
-            </div>
-        </aside>
+        <?php
+            $current_page = 'riwayat';
+            include 'includes/sidebar.php'; 
+        ?>
 
         <main class="main-content">
             
